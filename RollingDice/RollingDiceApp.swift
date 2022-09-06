a15d9b8a4ac1c7f148ce422013ef74c19b6d4718
ZhouHaibo <haibo.zhou@bsci.com>
2022-09-06 16:07:59 +0800

    first commit

---
+++ RollingDice/RollingDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  RollingDiceApp.swift
+//  RollingDice
+//
+//  Created by ZhouHaibo on 2022/9/6.
+//
+
+import SwiftUI
+
+@main
+struct RollingDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
